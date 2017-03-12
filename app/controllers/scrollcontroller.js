6ea29f46d34d3d9c84a86d7086d37893893c3322
Muraliduke <muraliduke4461@gmail.com>
2017-03-12 10:48:33 +0530

    Add files via upload

---
+++ app/controllers/scrollcontroller.js
@@ -0,0 +1,22 @@
+/*scroll controller for back to top*/
+angularMFRP.controller('ScrollController', function($scope, $location, $anchorScroll, $window) {
+    $scope.scrollFlag = false;
+    $scope.scrollup = function(loc) {
+		console.log(loc);
+        $location.hash(loc);
+		console.log($location.hash(loc))
+        $anchorScroll();
+    }
+    angular.element($window).bind("scroll", function() {
+        if (this.pageYOffset > 200) {
+
+            $scope.scrollFlag = true;
+        } else {
+            $scope.scrollFlag = false;
+        }
+        $scope.$apply()
+    });
+
+})
+
+/*scroll controller for back to top*/
